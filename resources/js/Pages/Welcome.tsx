7233c648a35cbf3bd77299d74797a157a23a1878
Delos-343 <mfachry.handoko@gmail.com>
2023-12-04 15:49:08 +0700

    Added scroll trigger function

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,8 +1,24 @@
 import { Link, Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
-import { Hero, Navbar } from '@/Components';
+import { Hero, Loader, Navbar, ScrollTriggerProxy } from '@/Components';
+import { AnimatePresence } from "framer-motion";
+import { useRef, useState, useEffect } from "react";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "@/Styles/GlobalStyles";
+import { dark } from "@/Styles/Themes";
 
 export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<{ laravelVersion: string, phpVersion: string }>) {
+
+    const containerRef = useRef(null);
+    const [Loaded, setLoaded] = useState(false);
+  
+    useEffect(() => {
+      setTimeout(() => {
+        setLoaded(true);
+      }, 3000);
+    }, []);
+
     return (
         <>
             <Head title="Welcome" />
@@ -85,6 +101,42 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<
             </div>
 
             <>
+                <GlobalStyles />
+                <ThemeProvider theme={dark}>
+                    <LocomotiveScrollProvider
+                    options={{
+                        smooth: true,
+                        // ... all available Locomotive Scroll instance options
+                        smartphone: {
+                        smooth: true,
+                        },
+                        tablet: {
+                        smooth: true,
+                        },
+                    }}
+                    watch={
+                        [
+                        //..all the dependencies you want to watch to update the scroll.
+                        //  Basicaly, you would want to watch page/location changes
+                        //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+                        ]
+                    }
+                    containerRef={containerRef}
+                    >
+                    <AnimatePresence>
+                        {Loaded ? null : <Loader />}
+                    </AnimatePresence>
+                    <main className="App" data-scroll-container ref={containerRef}>
+                        <ScrollTriggerProxy />
+                        <AnimatePresence>
+                            {Loaded ? null : <Loader />}
+
+                            <Navbar key="navbar" />
+                            <Hero key="home" />
+                        </AnimatePresence>
+                    </main>
+                    </LocomotiveScrollProvider>
+                </ThemeProvider>
                 <Navbar />
                 <Hero />
             </>
