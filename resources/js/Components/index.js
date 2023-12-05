883d6bf52ae6222a2c2a50676678a253fdd22264
Delos-343 <mfachry.handoko@gmail.com>
2023-12-05 15:04:57 +0800

    reinstalled Styled-Components

--- resources/js/Components/index.js
+++ resources/js/Components/index.js
@@ -1,3 +1,4 @@
 export { default as Loader } from './Loader';
 export { default as ImageHolder } from './ImageHolder';
+export { default as IconHolder } from './IconHolder';
 export { default as Hero } from './Hero';
